2e8ac171e495810bc1d2f2bb46005a529695cabc
Lisa Ong <onglisa@microsoft.com>
2021-11-30 12:09:54 +0800

    try export command

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
 REM ideally, this should be a submodule of Accera
 ..\Accera\external\vcpkg\vcpkg.exe install accera-llvm:x64-windows --overlay-ports=ports\accera-llvm
+..\Accera\external\vcpkg\vcpkg.exe export accera-llvm:x64-windows --zip
--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #/bin/sh
 #  ideally, this should be a submodule of Accera
 ../Accera/external/vcpkg/vcpkg install accera-llvm --overlay-ports=ports/accera-llvm
+../Accera/external/vcpkg/vcpkg export accera-llvm --zip
