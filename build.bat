abded35f7811ebc0af004b75a5c7055058239366
Lisa Ong <onglisa@microsoft.com>
2021-11-30 12:14:15 +0800

    7zip instead of zip

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
 REM ideally, this should be a submodule of Accera
 ..\Accera\external\vcpkg\vcpkg.exe install accera-llvm:x64-windows --overlay-ports=ports\accera-llvm
-..\Accera\external\vcpkg\vcpkg.exe export accera-llvm:x64-windows --zip
+
+REM -7zip is recommended for CMake projects (cf. https://vcpkg.readthedocs.io/en/latest/users/integration/#export-command)
+..\Accera\external\vcpkg\vcpkg.exe export accera-llvm:x64-windows --7zip
--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #/bin/sh
 #  ideally, this should be a submodule of Accera
 ../Accera/external/vcpkg/vcpkg install accera-llvm --overlay-ports=ports/accera-llvm
-../Accera/external/vcpkg/vcpkg export accera-llvm --zip
+
+# -7zip is recommended for CMake projects (cf. https://vcpkg.readthedocs.io/en/latest/users/integration/#export-command)
+../Accera/external/vcpkg/vcpkg export accera-llvm --7zip
