492dee73dcfff4189165f4ee0234a51ebf03134b
Lisa Ong <onglisa@microsoft.com>
2021-12-01 18:52:11 +0800

    fix scripts

--- build.bat
+++ build.bat
@@ -1,8 +1,6 @@
-REM external\vcpkg\vcpkg.exe install accera-llvm:x64-windows --overlay-ports=ports\accera-llvm
-external\vcpkg\vcpkg.exe install accera-mlir:x64-windows --overlay-ports=ports\accera-mlir
-REM external\vcpkg\vcpkg.exe install accera-lld:x64-windows --overlay-ports=ports\accera-lld
+external\vcpkg\bootstrap-vcpkg.bat
+
+external\vcpkg\vcpkg.exe install accera-llvm:x64-windows --overlay-ports=ports\accera-llvm
 
 REM -7zip is recommended for CMake projects (cf. https://vcpkg.readthedocs.io/en/latest/users/integration/#export-command)
-REM external\vcpkg\vcpkg.exe export accera-llvm:x64-windows --7zip
-external\vcpkg\vcpkg.exe export accera-mlir:x64-windows --7zip
-REM external\vcpkg\vcpkg.exe export accera-lld:x64-windows --7zip
+external\vcpkg\vcpkg.exe export accera-llvm:x64-windows --7zip
--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #/bin/sh
-# external/vcpkg/vcpkg install accera-llvm --overlay-ports=ports/accera-llvm
-external/vcpkg/vcpkg install accera-mlir --overlay-ports=ports/accera-mlir
-# external/vcpkg/vcpkg install accera-lld --overlay-ports=ports/accera-lld
+
+external/vcpkg/bootstrap-vcpkg.sh
+
+external/vcpkg/vcpkg install accera-llvm --overlay-ports=ports/accera-llvm
 
 # -7zip is recommended for CMake projects (cf. https://vcpkg.readthedocs.io/en/latest/users/integration/#export-command)
-# external/vcpkg/vcpkg export accera-llvm --7zip
-external/vcpkg/vcpkg export accera-mlir --7zip
-# external/vcpkg/vcpkg export accera-lld --7zip
+external/vcpkg/vcpkg export accera-llvm --7zip
